870281ebfc0e34df97f80b6a1a08eec9bf7c8549
kouki.nakase <yomkh304@gmail.com>
2020-08-25 14:15:43 +0000

    3 post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc)
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render "tasks/index"
+      render :new
     end
   end
 
